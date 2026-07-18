33937be56622346567a6931bd54a3c0b6ea1b6cb
anasanas876 <okashaanas986@gmail.com>
2026-07-17 21:16:29 -0700

    Models.py and Admin.Py file Updated

--- members/admin.py
+++ members/admin.py
@@ -1,3 +1,8 @@
+
+
+from .models import Project, Task
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Project)
+admin.site.register(Task)
--- members/models.py
+++ members/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+class Project(models.Model):
+    status_choices=[("CP", "Completed"),
+                    ("NS","Not Started"),
+                    ("IP","In Progress"),
+                    ("CA","Cancelled")
+                    ]
+    name= models.CharField(max_length=100)
+    description=models.CharField(max_length=500)
+    start_date=models.IntegerField()
+    status=models.CharField(max_length=2,choices=status_choices)
+   
+
+class Task(models.Model):
+    status_choices=[("CP", "Completed"),
+                    ("NS","Not Started"),
+                    ("IP","In Progress")
+                    
+                    ]
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    name=models.CharField(max_length=100)
+    description=models.CharField(max_length=100)
+    start_date=models.IntegerField()
+    status=models.CharField(max_length=2,choices=status_choices)
+
+
 
 # Create your models here.

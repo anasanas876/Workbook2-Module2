3c4bd617b936913425120aaa6d8071ee5895303b
Okasha Anas <b23f0542ai156@paf-iast.edu.pk>
2026-09-21 23:36:47 +0500

    Rebuilt logic for skills related tasks

--- members/files.py
+++ members/files.py
@@ -19,6 +19,100 @@ nltk.data.path = [
     r"C:\Users\B23F0542AI156\nltk_data"
 ]
 
+target_skills = [
+            "Python",
+            "Django",
+            "SQL",
+            "AWS",
+            "React",
+            "Javascript",
+            "MongoDB",
+            "Git",
+            "Artificial Intelligence",
+            "Data Science",
+]
+
+
+
+def skills_check(get_content):
+
+    skills = [
+        "Python",
+        "Django",
+        "SQL",
+        "AWS",
+        "React",
+        "Javascript",
+        "MongoDB",
+        "Git",
+        "Artificial Intelligence",
+        "Data Science",
+        "Azure"
+    ]
+
+    detected_skills = []
+
+    skills_section = [
+        "skills",
+        "technical skills",
+        "professional skills"
+    ]
+
+    inside_skills = False
+
+    for paragraph in get_content.paragraphs:
+
+        text = paragraph.text.strip()
+
+        if not text:
+            continue
+
+        if text.lower().rstrip(":") in skills_section:
+            inside_skills = True
+            continue
+
+        
+        if inside_skills:
+
+            # Using Section-based logic for skills extraction
+            if paragraph.style.name.startswith("Heading 1"):
+                inside_skills = False
+                continue
+
+            # Check skills in this paragraph
+            for skill in skills:
+
+                if re.search(
+                    rf"\b{re.escape(skill)}\b",
+                    text,
+                    re.IGNORECASE
+                ):
+                    if skill not in detected_skills:
+                        detected_skills.append(skill)
+
+    return detected_skills
+
+
+
+def list_missing_skills(target_skills,detected_skills):
+    target_skills = [
+            "Python",
+            "Django",
+            "SQL",
+            "AWS",
+            "React",
+            "Javascript",
+            "MongoDB",
+            "Git",
+            "Artificial Intelligence",
+            "Data Science",
+            "Azure"
+        ]
+    if target_skills not in detected_skills:
+        return list(target_skills)
+
+
+
 
 file_path = input("Enter your File Path: ")
 
@@ -52,209 +146,257 @@ try:
 
             print(extracted_text)
 
-       
-
         elif extension == "docx":
 
             get_content = Document(file_path)
-            extracted_text=""
-            for text in get_content.paragraphs.text():
-                extracted_text+=text
-                extracted_text=extracted_text.lower()
-                extracted_text=re.sub(r"[^a-zA-Z\d+@]"," ",extracted_text)
-                extracted_text=extracted_text.strip()
-
-            skills = [
-                "Python",
-                "Django",
-                "SQL",
-                "AWS",
-                "React",
-                "Javascript",
-                "MongoDB",
-                "Git",
-                "Artificial Intelligence",
-                "Data Science",
-                "Azure"
-            ]
-
-            detected_skills = []
-
-            skills_section = [
-                "Skills",
-                "Technical Skills",
-                "Professional Skills"
-            ]
-
-            # Convert section names to lowercase
-            skills_section = [
-                section.lower()
-                for section in skills_section
-            ]
-            find_skills=re.search("skills|technical skills|professional skills.(?=:\s+)",extracted_text)
-            print(find_skills)
-
-            print(
-                "Number of paragraphs:",
-                len(get_content.paragraphs)
-            )
-
             
-            paragraphs = get_content.paragraphs
-            text = ""
+            # calling a skills check function
+            detected_skills="skills_check"(get_content)
+            # Comparing detected skills against actual skills list
+            list_missing_skills(target_skills,skills_check())
+            # Designing a scoring function
+            list_missing_skills(target_skills,skills_check())
+
+    print("Detected Skills:", detected_skills)
+
+    # Comparing Detected Skills
+    
+
+
+
+        
 
             
 
-            for paragraph in paragraphs:
+#             print(
+#                 "Number of paragraphs:",
+#                 len(get_content.paragraphs)
+#             )
 
-                text += paragraph.text + "\n"
+            
+#             paragraphs = get_content.paragraphs
+#             text = ""
+
+            
+
+#             for paragraph in paragraphs:
+
+#                 text += paragraph.text + "\n"
 
            
 
-            for i, paragraph in enumerate(paragraphs):
+#             for i, paragraph in enumerate(paragraphs):
 
-                paragraph_text = paragraph.text.strip().lower()
+#                 paragraph_text = paragraph.text.strip().lower()
 
-                if (
-                    paragraph.style.name.startswith("Heading")
-                    and paragraph_text in skills_section
-                ):
+#                 if (
+#                     paragraph.style.name.startswith("Heading")
+#                     and paragraph_text in skills_section
+#                 ):
 
-                    print(
-                        "Section:",
-                        paragraph.text,
-                        ">",
-                        paragraph.style.name
-                    )
+#                     print(
+#                         "Section:",
+#                         paragraph.text,
+#                         ">",
+#                         paragraph.style.name
+#                     )
 
-                    # Read paragraphs after Skills heading
-                    for next_paragraph in paragraphs[i + 1:]:
+#                     # Read paragraphs after Skills heading
+#                     for next_paragraph in paragraphs[i + 1:]:
 
-                        # Stop when another heading is reached
-                        if next_paragraph.style.name.startswith("Heading"):
-                            break
+#                         # Stop when another heading is reached
+#                         if next_paragraph.style.name.startswith("Heading"):
+#                             break
 
-                        paragraph_text = (
-                            next_paragraph.text.strip().lower()
-                        )
+#                         paragraph_text = (
+#                             next_paragraph.text.strip().lower()
+#                         )
 
-                        # Check skills only inside Skills section
-                        for skill in skills:
+#                         # Check skills only inside Skills section
+#                         for skill in skills:
 
-                            if skill.lower() in paragraph_text:
+#                             if skill.lower() in paragraph_text:
 
-                                if skill not in detected_skills:
-                                    detected_skills.append(skill)
+#                                 if skill not in detected_skills:
+#                                     detected_skills.append(skill)
 
-            print(
-                "Detected Skills:",
-                detected_skills
-            )
-            # Designing Rubric System
-            skills_percentage=40
-            experience=30
-            keywords=30
-            calculate_score=len(detected_skills)/len(skills)*100
-            print(calculate_score)
+#             print(
+#                 "Detected Skills:",
+#                 detected_skills
+#             )
+#             # Designing Rubric System
+#             skills_percentage=40
+#             experience=30
+#             keywords=30
+#             calculate_score=len(detected_skills)/len(skills)*100
+#             print(calculate_score)
 
-            # Getting Experience
-            experience_section=["Professional Experience",
-                                "Technical Experience","Experience"]
-            experience_section=[experience.lower() for experience in experience_section]
-            for i,paragraph in enumerate(paragraphs):
-                if (paragraph.style.name.startswith("Heading 1") and paragraph.text in experience_section):
-                    for get_experience in paragraph[i+1:]:
-                        if get_experience.style.name.startswith("Heading 1"):
-                            break
-                        record_experience=get_experience.text
-                        experience_years=nlp(record_experience)
-                        print(experience_years.ents)
+#             # Getting Experience
+#             experience_section=["Professional Experience",
+#                                 "Technical Experience","Experience"]
+#             experience_section=[experience.lower() for experience in experience_section]
+#             for i,paragraph in enumerate(paragraphs):
+#                 if (paragraph.style.name.startswith("Heading 1") and paragraph.text in experience_section):
+#                     for get_experience in paragraph[i+1:]:
+#                         if get_experience.style.name.startswith("Heading 1"):
+#                             break
+#                         record_experience=get_experience.text
+#                         experience_years=nlp(record_experience)
+#                         print(experience_years.ents)
                         
                             
 
 
-            text = text.lower().strip()
+#             text = text.lower().strip()
 
-            #print("Resume Text:")
-            #print(text)
+#             #print("Resume Text:")
+#             #print(text)
 
-            # Counting Words
-            print(
-                "Word Count:",
-                len(text.split())
-            )
-
-           
-            cleaned_text = re.sub(
-                r'[^a-zA-Z0-9\s]',
-                '',
-                text
-            )
-
-            print("Cleaned Text:")
-            #print(cleaned_text)
+#             # Counting Words
+#             print(
+#                 "Word Count:",
+#                 len(text.split())
+#             )
 
            
+#             cleaned_text = re.sub(
+#                 r'[^a-zA-Z0-9\s]',
+#                 '',
+#                 text
+#             )
 
-            tokens = word_tokenize(cleaned_text)
+#             print("Cleaned Text:")
+#             #print(cleaned_text)
 
-            print("Tokens:")
-            print(tokens)
+           
 
-            
+#             tokens = word_tokenize(cleaned_text)
 
-            stop_words = set(
-                stopwords.words("english")
-            )
-
-            filtered_tokens = [
-                word
-                for word in tokens
-                if word not in stop_words
-            ]
-
-            print("After Stopword Removal:")
-            print(filtered_tokens)
+#             print("Tokens:")
+#             print(tokens)
 
             
 
-            lemmatizer = WordNetLemmatizer()
+#             stop_words = set(
+#                 stopwords.words("english")
+#             )
 
-            lemmatized = [
-                lemmatizer.lemmatize(word)
-                for word in filtered_tokens
-            ]
+#             filtered_tokens = [
+#                 word
+#                 for word in tokens
+#                 if word not in stop_words
+#             ]
 
-            print("Lemmatized:")
-            print(lemmatized)
+#             print("After Stopword Removal:")
+#             print(filtered_tokens)
+
+            
+
+#             lemmatizer = WordNetLemmatizer()
+
+#             lemmatized = [
+#                 lemmatizer.lemmatize(word)
+#                 for word in filtered_tokens
+#             ]
+
+#             print("Lemmatized:")
+#             print(lemmatized)
 
             
 
             
 
-            doc = nlp(text)
+#             doc = nlp(text)
 
-            print("Named Entities:")
+#             print("Named Entities:")
 
-            for entity in doc.ents:
+#             for entity in doc.ents:
 
-                print(
-                    entity.text,
-                    ">",
-                    entity.label_
-                )
+#                 print(
+#                     entity.text,
+#                     ">",
+#                     entity.label_
+#                 )
 
         
 
-        else:
+#         else:
 
-            print(
-                "Not supported file type for Resume"
-            )
+#             print(
+#                 "Not supported file type for Resume"
+#             )
 
 
 except FileNotFoundError:
 
-    print(
-        "The path entered does not exist."
-    )
+        print(
+       "The path entered does not exist."
+   )
+
+
+# 
+def skills_check(get_content):
+
+    skills = [
+        "Python",
+        "Django",
+        "SQL",
+        "AWS",
+        "React",
+        "Javascript",
+        "MongoDB",
+        "Git",
+        "Artificial Intelligence",
+        "Data Science",
+        "Azure"
+    ]
+
+    detected_skills = []
+
+    skills_section = [
+        "skills",
+        "technical skills",
+        "professional skills"
+    ]
+
+    inside_skills = False
+
+    for paragraph in get_content.paragraphs:
+
+        text = paragraph.text.strip()
+
+        if not text:
+            continue
+
+        # Check whether this paragraph is a Skills heading
+        if text.lower().rstrip(":") in skills_section:
+            inside_skills = True
+            continue
+
+        # If we are inside Skills section
+        if inside_skills:
+
+            # Check if another major heading has started
+            if paragraph.style.name.startswith("Heading 1"):
+                inside_skills = False
+                continue
+
+            # Check skills in this paragraph
+            for skill in skills:
+
+                if re.search(
+                    rf"\b{re.escape(skill)}\b",
+                    text,
+                    re.IGNORECASE
+                ):
+                    if skill not in detected_skills:
+                        detected_skills.append(skill)
+
+    return detected_skills
+
+def score_resume(target_skills,detected_skills):
+    detected_skills=skills_check()
+    skills_count=target_skills/detected_skills
+    skills_count=skills_count*100
+    return skills_count
+
